90bf8e8935049c7e97dffcd193497bb74362d7ce
bipana chapain <chapai.bipana65@gmail.com>
2024-12-10 14:27:47 +0545

    first commit

---
+++ app/Http/Controllers/Product_Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Product_Controller extends Controller
+{
+    public function index(){
+        return view('product.index');
+    }
+    public function store(){
+        return view('');
+
+    }
+}
